2d03b9ff9e6c1bbfd1e17c9bb76fab23c7e05091
pradeepmondal <mail.pradeepmondal@gmail.com>
2022-01-19 12:57:33 +0530

    changes done

--- Week1/comp3ter.c
+++ Week1/comp3ter.c
@@ -5,7 +5,13 @@ int main(){
     printf("Enter the value of a b c: ");
     scanf("%d %d %d", &a, &b, &c);
     max = (a>b)?((a>c)?a:c):((b>c)?b:c);
+    if(a==b==c){
+        printf("Numbers are equal");
+    }
+    else{
     printf("%d is max", max);
+    }
+    
     return 0;
 
 }
